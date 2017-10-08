8afccccaee8ab350d3c0e33a8bc16798c1436538
ramesgitt <ramesh.mkrk@gmail.com>
2017-10-08 20:51:53 +0530

    mod-ser

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const bodyParser=require('body-parser')
+const{ObjectId}=require('mongodb')
 
 
 var{mongoose}=require('./db/mongoose')
@@ -51,6 +52,45 @@ app.get('/todos',(req,res)=>{
 })
 
 
+//app.get('/todos/:id',(req,res)=>{
+//   var id=req.params.id
+//   if(ObjectId.isValid(id)){
+//       Todo.findById(id).then((doc)=>{
+////        if(!doc){
+////            res.status(404).send()
+////        }
+//           res.send(doc)
+//           
+//       },(e)=>{
+//           res.status(400).send()
+//           
+//       })
+//       
+//   }else{
+//       res.status(404).send()
+//   }
+//    
+//})
+
+app.get('/todos/:id',(req,res)=>{
+    var id=req.params.id
+    if(!ObjectId.isValid(id)){
+        return res.status(404).send()
+    }
+    Todo.findById(id).then((todo)=>{
+        if(!todo){
+            return res.status(404).send()
+        }
+        res.send({todo})
+        
+    }).catch((e)=>{
+        res.status(400).send()
+    })
+    
+    
+    
+    
+})
 
 
 
